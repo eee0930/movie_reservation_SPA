ba965e92886912db3bf0ff0a6d4fd7b2785be305
lazyhysong <eee0930@naver.com>
2023-10-13 22:44:44 +0900

    refactor: 유효성 검사 코드 변경

--- src/components/HandleLogin.js
+++ src/components/HandleLogin.js
@@ -11,49 +11,59 @@ class HandleLogin {
     this.$btn.addEventListener("click", this.handleLogin);
   };
 
-  verifyEmail = (value) => {
+  verifyEmail = () => {
+    const value = this.$email.value;
     const regex = new RegExp("[a-zA-Z0-9.]+@[a-zA-Z0-9._-]+.co");
-    if (regex.test(value)) {
-      return true;
-    } else {
-      alert("이메일 형식이 올바르지 않습니다.");
-      return false;
-    }
+    return regex.test(value) ? true : false;
   };
 
-  verifyPassword = (value) => {
+  verifyPasswordSize = (min, max) => {
+    const value = this.$password.value;
+    const len = value.length;
+    return len < min || len > max ? false : true;
+  };
+
+  verifyPassword = () => {
+    const value = this.$password.value;
     const num = value.search(/[0-9]/g);
     const eng = value.search(/[A-Za-z]/gi);
     const spe = value.search(/[!@@~]/gi);
-    const len = value.length;
-    if (len < 8 || len > 20) {
-      alert("비밀번호는 최소 8자 이상, 최대 20자 이하로 구성해야 합니다.");
-      return false;
-    } else if (num < 0 || eng < 0 || spe < 0) {
-      alert("비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다.");
-      return false;
-    } else {
-      return true;
-    }
+    return num < 0 || eng < 0 || spe < 0 ? false : true;
+  };
+
+  verifyEnteredInFeild = () => {
+    return [this.$email, this.$password].every(
+      (input) => input.value.trim() !== ""
+    );
   };
 
   handleLogin = () => {
-    const email = this.$email.value;
-    const password = this.$password.value;
-    if (
-      email === "" ||
-      email.length < 1 ||
-      password === "" ||
-      password.length < 1
-    ) {
-      alert("이메일 혹은 비밀번호가 입력되지 않았습니다.");
-    } else {
-      if (this.verifyEmail(email) && this.verifyPassword(password)) {
-        alert("로그인 성공!");
-      } else {
+    const validations = [
+      {
+        fn: this.verifyEnteredInFeild,
+        errMsg: "아이디 혹은 비밀번호가 입력되지 않았습니다.",
+      },
+      {
+        fn: this.verifyEmail,
+        errMsg: "이메일 형식이 올바르지 않습니다.",
+      },
+      {
+        fn: this.verifyPasswordSize(8, 20),
+        errMsg: "비밀번호는 최소 8자 이상, 최대 20자 이하로 구성해야 합니다.",
+      },
+      {
+        fn: this.verifyPassword,
+        errMsg: "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다.",
+      },
+    ];
+
+    for (let validation of validations) {
+      if (!validation.fn.call(this)) {
+        alert(validation.errMsg);
         return;
       }
     }
+    alert("로그인 성공!");
   };
 }
 
